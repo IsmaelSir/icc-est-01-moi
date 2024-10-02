2821b633c029cb92ca9044bac3d899decda74a9b
IsmaelSir <https://github.com/IsmaelSir/ICC-EST-01-PRACTICAJAVA.git>
2024-10-02 12:25:51 -0600

    Practica finalizada

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //Instaciamos la clase
+        MetodoOrdenamientoInsercion ordenar = new MetodoOrdenamientoInsercion();
+        boolean imprimir = true;
+        int[] arreglo = {5,9,2,7,4,10};
+        ordenar.sortInsercion(arreglo, imprimir);
+
+        ordenar.inprimirArray(arreglo);
+    }
+}
